7818a9d37c1cda6d10af8477a1cf31d2939bd0f2
olivier <oliviermalik@yahoo.fr>
2019-02-11 10:26:48 +0100

    register view

--- src/main/java/com/miage/altea/tp/pokemon_ui/controller/IndexController.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/controller/IndexController.java
@@ -2,6 +2,8 @@ package com.miage.altea.tp.pokemon_ui.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
@@ -9,5 +11,12 @@ public class IndexController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @PostMapping("/registerTrainer")
+    ModelAndView registerNewTrainer(String trainerName){
+        var modelAndView = new ModelAndView("register");
+        modelAndView.addObject("name",trainerName);
+        return modelAndView;
     }
 }
