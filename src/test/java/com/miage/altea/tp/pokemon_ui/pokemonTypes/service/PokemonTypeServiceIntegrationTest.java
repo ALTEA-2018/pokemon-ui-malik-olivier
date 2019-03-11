96945df8ed7dcc6208f59c1f1c11aeb75f9ec8c2
olivier <oliviermalik@yahoo.fr>
2019-03-11 09:47:54 +0100

    ihm first version

---
+++ src/test/java/com/miage/altea/tp/pokemon_ui/pokemonTypes/service/PokemonTypeServiceIntegrationTest.java
@@ -0,0 +1,23 @@
+package com.miage.altea.tp.pokemon_ui.pokemonTypes.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.Assert.assertNotNull;
+
+@SpringBootTest
+public class PokemonTypeServiceIntegrationTest {
+    @Autowired
+    PokemonTypeService service;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Test
+    void serviceAndTemplateShouldNotBeNull(){
+        assertNotNull(service);
+        assertNotNull(restTemplate);
+    }
+}
