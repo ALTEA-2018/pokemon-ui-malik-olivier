62759a6af3c6a2cdb61c89fae7bf7899f8295a92
olivier <oliviermalik@yahoo.fr>
2019-04-05 10:05:07 +0200

    display arena

--- src/main/java/com/miage/altea/tp/pokemon_ui/config/SecurityConfig.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/config/SecurityConfig.java
@@ -1,12 +1,11 @@
 package com.miage.altea.tp.pokemon_ui.config;
 
-import com.miage.altea.tp.pokemon_ui.pokemonTypes.service.TrainerDetailsService;
 import com.miage.altea.tp.pokemon_ui.pokemonTypes.service.TrainerService;
 import com.miage.altea.tp.pokemon_ui.pokemonTypes.service.impl.TrainerDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,6 +25,12 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
         var details =   new TrainerDetailsServiceImpl();
         details.setTrainerService(trainerService);
         return details;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().ignoringAntMatchers("/api/**");
     }
 
     @Autowired
